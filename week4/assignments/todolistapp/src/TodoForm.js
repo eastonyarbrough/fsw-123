c6c1670736885059c149f56e22587b80ac8f017e
Easton Yarbrough <easton.yarbrough@bryanuniversity.edu>
2021-12-15 01:40:07 -0600

    Complete 'Todo Part 4: Adding' assignment

--- week4/assignments/todolistapp/src/TodoForm.js
+++ week4/assignments/todolistapp/src/TodoForm.js
@@ -8,9 +8,24 @@ export default function TodoForm(props) {
     }
     
     return(
-        <div>
+        <div id="inputCont">
             <input type="text" placeholder="Please enter Todo" id="inputValue" onChange={() => changeState()}></input>
-            <button onClick={() => props.addTodo(userInput)}>Submit</button>
+            <button onClick={() => {
+                if (document.querySelector("#inputValue").value !== "") {
+                    props.addTodo(userInput)
+                }
+                else if (document.querySelector("#inputValue").value === "") {
+                    const removeErr = () => {
+                        document.querySelector("#errMsg").remove();
+                    }
+                    const errMsg = document.createElement("h3");
+                    errMsg.textContent = "Input field is empty";
+                    errMsg.id = "errMsg";
+                    errMsg.style = "color: red"
+                    document.querySelector("#inputCont").appendChild(errMsg);
+                    setTimeout(removeErr, 1500);
+                }
+            }}>Submit</button>
         </div>
     );
 };
